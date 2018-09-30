28fd433e6ccde68d5b5b73010f0d5293770b3d52
hpjaj <hmlevine@gmail.com>
2018-09-29 20:45:10 -0600

    Collects a user’s GitHub username for basic auth

---
+++ db/migrate/20180930023651_add_git_hub_username_to_user.rb
@@ -0,0 +1,5 @@
+class AddGitHubUsernameToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :git_hub_username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180929203739) do
+ActiveRecord::Schema.define(version: 20180930023651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180929203739) do
     t.string "encrypted_personal_access_token_iv"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "git_hub_username"
   end
 
 end
