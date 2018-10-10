86496c2ee5c16d18cd0c1daea03ce262ed5fa231
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2018-10-09 20:30:06 -0700

    wires in a basic react rails setup

--- README.md
+++ README.md
@@ -8,4 +8,12 @@ React-rails is configured, without react router currently
 
 All components are visible in `app/javascript/components`
 
+I've followed the paradigm introduced by Dan Abramov back in 2015 around smart and dumb components. Further reading on this topic is available [here](https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0)
+
+* To generate a react component *
+
+`rails g react:component $COMPONENT_NAME`
+
+Currently there is no connection from React to Rails, in a seperate PR, once the graphql stuff is implemented we can connect the apollo-provider to our graphql endpoint.
+
 Currently using regular old css, but I brought in [Tail Wind CSS](https://tailwindcss.com/docs/what-is-tailwind/) to make things a little easier
--- app/javascript/components/Hello.js
+++ app/javascript/components/Hello.js
@@ -1,49 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import gql from 'graphql-tag';
-import { Query } from "react-apollo";
 
-const Login = () => {
-  return (
-      <span>
-      <a href="/auth/google_oauth2">Please login by going here</a>
-      </span>
-  )
-}
-const Logout = () => (
-  <span>
-  <a href="/signout">Signout</a>
-  </span>
-
-)
-
-const TEST_QUERY = gql`
- {
-   me {
-     name
-   }
- }
-`;
 
 class Hello extends React.Component {
   render() {
     return (
-      <Query query={TEST_QUERY} >
-      {({ loading, error, data }) => {
-        if (loading) return "Loading";
-        if (error) return `Error!: ${error}`;
-        if(!data.me)  {
-          return <Login />
-        }
-        return (
-          <div>
-            <h1 className="hello">Hello {data.me.name}!</h1>
-            <Logout />
-          </div>
-        )
-      }}
-      </Query>
+      <div>
+        <h1 className="hello">Hello World!</h1>
+      </div>
     );
   }
 } 
