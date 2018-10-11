c1b8c85a6309591c35983515c1e73f5fc98e5c7a
Keifer Furzland <kfrz@users.noreply.github.com>
2018-10-11 12:10:58 -0500

    Move root.html.erb to index.html.erb (#33)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// Support component names relative to this directory:
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import "../css/application.css";
 
 import React from "react";
