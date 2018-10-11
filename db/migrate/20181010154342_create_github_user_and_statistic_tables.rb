0969fce2259a00769171162dfe7ec3bae0a54516
hpjaj <hmlevine@gmail.com>
2018-10-10 20:06:11 -0600

    New models’ migrations and schema

---
+++ db/migrate/20181010154342_create_github_user_and_statistic_tables.rb
@@ -0,0 +1,36 @@
+class CreateGithubUserAndStatisticTables < ActiveRecord::Migration[5.1]
+  def change
+    create_table :github_users do |t|
+      t.integer :user_id
+      t.string :github_login
+      t.string :avatar_url
+      t.string :api_url
+      t.string :html_url
+      t.integer :github_id
+      t.boolean :oddball_employee, null: false, default: false
+
+      t.timestamps
+    end
+
+    create_table :statistics do |t|
+      t.string :source_id
+      t.string :source_type
+      t.string :source
+      t.string :state
+      t.integer :repository_id
+      t.references :organization, foreign_key: true
+      t.string :url
+      t.string :title
+      t.string :source_created_at
+      t.string :source_updated_at
+      t.string :source_closed_at
+
+      t.timestamps
+    end
+
+    create_table :github_users_statistics, id: false do |t|
+      t.belongs_to :github_user, index: true
+      t.belongs_to :statistic, index: true
+    end
+  end
+end
