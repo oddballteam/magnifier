b358b4961dbf16079f80275fe53dd4f16d715c9d
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2018-10-12 13:51:00 -0700

    Migrate to credentials (#37)
    * Increase max line length to 120 char
    
    * Update ruby version to 2.5.1
    
    * Update rails to 5.2.1
    
    * Remove secrets.yml
    * Update configuration files and scripts accordingly
    * Add content_security_policy in place of protect_from_forgery
    * Upgrade gems as necessary
    * See: http://railsdiff.org/5.1.5/5.2.1
    
    * Add credentials file
    
    * bring things into rails credentials format
    
    * bad links in applicatio template
    
    * remove unneeded js from app/assets
    
    * Save key as hex, and unpack into 32 bytes
    
    As we are unable to save binary in credentials file, save key as
    hexidecimal and in model unpack into binary.
    
    * rails credential key

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@
 #
 class User < ApplicationRecord
   # @see https://github.com/attr-encrypted/attr_encrypted#usage
-  #
+  # @see https://github.com/attr-encrypted/attr_encrypted/issues/311
   attr_encrypted :personal_access_token,
-                 key: Rails.application.secrets.encryption_key,
+                 key: Rails.application.credentials.encryption_key,
                  encode: true
 
   validates :first_name, :last_name, presence: true
