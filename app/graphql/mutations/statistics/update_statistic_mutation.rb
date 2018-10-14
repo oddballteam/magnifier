389740398ba2b9e87f5d2b9db08da49b37a3cd6d
hpjaj <hmlevine@gmail.com>
2018-10-14 17:23:04 -0600

    Accounts for a Statistic not being found during update

--- app/graphql/mutations/statistics/update_statistic_mutation.rb
+++ app/graphql/mutations/statistics/update_statistic_mutation.rb
@@ -7,7 +7,7 @@ module Mutations
       null true
 
       argument :attributes, Types::StatisticAttributes, required: true
-      argument :id, ID, required: true
+      argument :id, Integer, required: true
 
       field :statistic,
             Types::StatisticType,
@@ -16,16 +16,23 @@ module Mutations
       field :errors, [String], null: true
 
       def resolve(attributes:, id:)
-        args      = attributes.to_h.symbolize_keys
-        statistic = ::Statistic.find_by id: id.to_i
+        statistic = ::Statistic.find_by id: id
 
-        statistic.update! args
-        statistic_response statistic.reload
+        if statistic.present?
+          statistic.update! args_for(attributes)
+          statistic_response statistic.reload
+        else
+          statistic_response nil, ["Statistic with id #{id} does not exist"]
+        end
       rescue ActiveRecord::RecordInvalid => e
-        statistic_response(nil, e.record.errors.full_messages)
+        statistic_response nil, e.record.errors.full_messages
       end
 
       private
+
+      def args_for(attributes)
+        attributes.to_h.symbolize_keys
+      end
 
       def statistic_response(statistic, error_messages = [])
         {
