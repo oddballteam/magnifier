fe5535947a1e76a2ef31425d2701438f2c975640
hpjaj <hmlevine@gmail.com>
2018-10-14 10:11:16 -0600

    Favors new manifest based implementation over this one

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 module Types
-  # All logic for the individual queries lies in the included
-  # modules.  For example, all of the query logic for Organizations
-  # resides in the Types::QueryTypes::Organization module.
-  #
   class QueryType < Types::BaseObject
-    include Types::QueryTypes::GithubUser
-    include Types::QueryTypes::Organization
-    include Types::QueryTypes::Repository
-    include Types::QueryTypes::Statistic
   end
 end
