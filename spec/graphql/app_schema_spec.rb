37cb723d474f3fdae1c6b5b87bfd65cd8793000d
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2018-10-17 17:48:58 -0700

    Rake task export graphql schema (#40)
    * adds a small task and test to update schema
    
    * adds graphql schema
    
    * tabs
    
    * formatting

---
+++ spec/graphql/app_schema_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe do
+  it "has an up to date .graphql file" do
+    current_defn = MagnifierSchema.to_definition
+    printout_defn = File.read(Rails.root.join("app/graphql/schema.graphql"))
+    expect(current_defn).to eq(printout_defn), "Update the printed schema with `bundle exec rake dump_schema`"
+  end
+end
