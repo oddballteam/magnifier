65afd2ea06d457d0282444657c3600cd41360175
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2018-10-17 17:48:29 -0700

    formatting

--- spec/graphql/app_schema_spec.rb
+++ spec/graphql/app_schema_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe do
   it "has an up to date .graphql file" do
     current_defn = MagnifierSchema.to_definition
     printout_defn = File.read(Rails.root.join("app/graphql/schema.graphql"))
-    assert_equal(current_defn, printout_defn, "Update the printed schema with `bundle exec rake dump_schema`")
+    expect(current_defn).to eq(printout_defn), "Update the printed schema with `bundle exec rake dump_schema`"
   end
 end
