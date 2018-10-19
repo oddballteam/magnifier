3881597aa375b1f31a85d11492f0de4b51e68915
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2018-10-19 16:16:24 -0700

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   attr_encrypted :personal_access_token,
                  key: Rails.application.credentials.encryption_key,
                  encode: true
-
+  validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 
   def self.from_omniauth(auth)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,9 +9,9 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
 
-    # it { should validate_presence_of(:first_name) }
-    # it { should validate_presence_of(:last_name) }
-    # it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
     # it { should validate_presence_of(:github_username) }
   end
   describe 'from_omniauth' do
