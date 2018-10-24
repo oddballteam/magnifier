a212f33b35d1737778377827b93f9f9252f468ee
hpjaj <hmlevine@gmail.com>
2018-10-24 11:09:44 -0600

    A user will need an organization to pull GitHub data
    However, we cannot make this a direct Rails association, b/c we cannot validate for the presence of it when the user is being created.
    
    Reason being, we are using OAuth to initially create the user, and at that point of the user being created, they will not yet have an GitHub organization associated with their record.

---
+++ db/migrate/20181020225546_add_organization_references_to_user.rb
@@ -0,0 +1,6 @@
+class AddOrganizationReferencesToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :organization_id, :integer
+    add_index :users, :organization_id
+  end
+end
