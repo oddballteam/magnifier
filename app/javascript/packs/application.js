67c35bd33d92ec155d3fc3c16664cf1995e1b226
hpjaj <hmlevine@gmail.com>
2018-12-02 19:33:54 -0700

    Integrates Apollo client for GraphQL calls

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,20 @@ import Application from "../containers/Application";
 import routes from "../routes";
 import "../css/application.css";
 
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: '/graphql'
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
-    <Router>
-      <Application>{routes}</Application>
-    </Router>,
+    <ApolloProvider client={client}>
+      <Router>
+        <Application>{routes}</Application>
+      </Router>
+    </ApolloProvider>,
     document.getElementById("root")
   );
 });
