9b8544df17cf55e066f6bae848c3e07de6978b7e
hpjaj <hmlevine@gmail.com>
2018-12-09 10:02:28 -0700

    Associations on User and Statistic

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
                  encode: true
 
   has_many :week_in_reviews
+  has_many :comments
+  has_many :accomplishments
+
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :github_username, uniqueness: true
