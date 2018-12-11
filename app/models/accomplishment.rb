9fe9a20c2a45ce94d3a328e6677c413fc016b34e
hpjaj <hmlevine@gmail.com>
2018-12-11 08:24:59 -0700

    Enables :type column by disabling STI
    Rails uses the :type column for single table inheritance, by default.  If we want to use it for another purpose, it needs to be disabled as such.
    
    @see https://apidock.com/rails/ActiveRecord/Base/inheritance_column/class#1045-Disable-STI

--- app/models/accomplishment.rb
+++ app/models/accomplishment.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Accomplishment < ApplicationRecord
+  # Disables single table inheritance
+  # @see https://apidock.com/rails/ActiveRecord/Base/inheritance_column/class#1045-Disable-STI
+  #
+  self.inheritance_column = :_type_disabled
+
   # @see https://api.rubyonrails.org/v5.2.1/classes/ActiveRecord/Enum.html
   #
   enum action: { created: 0, worked: 1, closed: 2, merged: 3 }
