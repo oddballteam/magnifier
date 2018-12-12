3e98fa21d0f019022804e3cd105f3f83d71acd95
hpjaj <hmlevine@gmail.com>
2018-12-11 20:53:56 -0700

    Extracts StatsGroup for use in other components
    this way, we can use the same presentation in both the Statistics page, and the week in review page

--- app/javascript/components/StatisticsCollection.js
+++ app/javascript/components/StatisticsCollection.js
@@ -2,6 +2,25 @@ import React from "react";
 import { Query } from "react-apollo";
 import Statistic from "./Statistic";
 
+const StatisticsGroup = (statistics, showHeader, title) => {
+  return (
+    <div className="flex flex-wrap flex-col">
+      {showHeader ? (
+        <h3 className="py-8 px-0">{`${title} (${statistics.length})`}</h3>
+      ) : (
+        ""
+      )}
+      <div>
+        {statistics
+          ? statistics.map(statistic => (
+              <Statistic {...statistic} key={statistic.sourceCreatedAt} />
+            ))
+          : ""}
+      </div>
+    </div>
+  );
+};
+
 const StatisticsCollection = ({ customQuery, githubUserId, date, title }) => (
   <Query
     query={customQuery}
@@ -11,27 +30,13 @@ const StatisticsCollection = ({ customQuery, githubUserId, date, title }) => (
     {({ loading, error, data }) => {
       if (loading) return <p>Loading...</p>;
       if (error) return <p>Error!</p>;
-      if (data && data.statistics)
-        return (
-          <div className="flex flex-wrap flex-col">
-            {githubUserId ? (
-              <h3 className="py-8 px-0">{`${title} (${
-                data.statistics.length
-              })`}</h3>
-            ) : (
-              ""
-            )}
-            <div>
-              {data.statistics.map(statistic => (
-                <Statistic {...statistic} key={statistic.sourceCreatedAt} />
-              ))}
-            </div>
-          </div>
-        );
-
+      if (data && data.statistics) {
+        return StatisticsGroup(data.statistics, githubUserId, title);
+      }
       return <div />;
     }}
   </Query>
 );
 
 export default StatisticsCollection;
+export { StatisticsGroup };
