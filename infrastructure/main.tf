ca845cbdd3f69df1a902fdba79e44092949cf22b
Rob Wilkinson <1959628+RobAWilkinson@users.noreply.github.com>
2019-09-12 11:10:02 -0400

    update version

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
-  version    = "~> 1.56.0"
+  version    = "~> 2.7"
 }
 
 terraform {
